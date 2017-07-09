ade2abc5ceb027ee7b055cd1a4850c349d7fceb2
Sol Cates <scates@vormetric.com>
2017-07-09 10:35:45 -0700

    initial commit

---
+++ restapi/doc.go
@@ -0,0 +1,22 @@
+/*Package restapi med-tracker
+
+Med Tracker for keeping dosages straight when caring for your wife :)
+
+
+    Schemes:
+      http
+    Host: localhost
+    BasePath: /api
+    Version: 1.0.0
+
+    Consumes:
+    - application/json
+
+
+    Produces:
+    - application/json
+
+
+swagger:meta
+*/
+package restapi
